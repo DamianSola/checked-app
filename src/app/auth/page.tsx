4e136ed23f1ceefbca15c8f65f0586446122d95d
damian sola <damiansola99@gmail.com>
2024-11-14 18:31:30 -0300

    estilo

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -7,7 +7,7 @@ const Auth = () => {
     const [login, setLogin] = useState(false)
 
     return(
-        <div className="md:flex bg-gray-900">
+        <div className="md:flex bg-gray-900 text-gray-100 h-screen">
             {login ? 
             <Register changeLogin = {() => setLogin(!login)}/> :
             <Login changeRegister = {() => setLogin(!login)}/>
